af82012ef4eea523b30d745530b1026524f0d686
Bushra Mehmood Khan <bushramehmood797@gmail.com>
2024-03-19 22:26:21 +0500

    second commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "firstNumber" },
+    { message: "Enter second number", type: "number", name: "secondNumber" },
+    {
+        message: "Select on eof the operators to perform action", type: "list",
+        name: "operator",
+        choices: ["Addition", "Susbtraction", "Multiplication", "Division"],
+    }
+]);
+//conditional statment
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "Substraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
